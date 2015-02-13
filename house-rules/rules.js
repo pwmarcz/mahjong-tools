946a636c876b4933db015b568cd509bf4492eb7a
Paweł Marczewski <pawel@codility.com>
2015-02-13 20:24:49 +0100

    add languages support

--- house-rules/rules.js
+++ house-rules/rules.js
@@ -1,15 +1,45 @@
+
+var LANGUAGES = ['en', 'pl'];
+
 function prepare() {
     var rules = jsyaml.load($('#rules-yaml').html());
     var table = $('#rules-table');
     $.each(rules, function(i, rule) {
         table.append(renderRule(i, rule));
     });
+
+    $('input[name=language]').change(updateLanguage);
+    updateLanguage();
+}
+
+function updateLanguage() {
+    $.each(LANGUAGES, function(i, lang) {
+        $('.lang-' + lang).hide();
+    });
+
+    var lang = $('input[name=language]:checked').val();
+    $('.lang-' + lang).show();
+}
+
+function forLanguages(content, func) {
+    if (typeof content === 'string') {
+        func(content, 'all');
+    } else {
+        $.each(LANGUAGES, function(i, lang) {
+            if (content[lang]) {
+                func(content[lang], lang);
+            }
+        });
+    }
 }
 
 function renderRule(i, rule) {
     var src = '<tr><td class="name"></td><td class="options"></td></tr>';
     var elt = $(src);
-    elt.find('.name').append(hepburn(rule.name));
+    forLanguages(rule.name, function(name, lang) {
+        var name_in_lang = $('<div>').addClass('lang-'+lang).text(name);
+        elt.find('.name').append(name_in_lang);
+    });
     var options = elt.find('.options');
     if (rule.select) {
         $.each(rule.select, function(j, val) {
@@ -30,7 +60,6 @@ function renderRule(i, rule) {
 }
 
 function renderOpt(type, i, j, val, initial) {
-    val = hepburn(val);
     var id = i+'-'+j;
     var name;
     if (type == 'radio')
@@ -40,10 +69,15 @@ function renderOpt(type, i, j, val, initial) {
     var elt = $(
         '<div>' +
         '<input class="opt" type="'+type+'" name="'+name+'" id="'+id+'">' +
-        '<label for="'+id+'">'+val+'</label>' +
+        '<label for="' + id + '"></label>' +
         '</div>'
     );
-    if (initial == undefined)
+
+    forLanguages(val, function(description, lang) {
+        var label_in_lang = $('<span>').addClass('lang-'+lang).attr('for', id).text(description);
+        elt.find('label').append(label_in_lang);
+    });
+    if (initial === undefined)
         initial = [1];
     if (typeof initial == 'number')
         initial = [initial];
